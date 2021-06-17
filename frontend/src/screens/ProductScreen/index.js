8727f8f709d3abed5339af8a3386a73690e34f23
Samuel Felipe <samuelfelipef@gmail.com>
2021-06-17 18:22:08 -0300

    add link back to results in ProductScreen

--- frontend/src/screens/ProductScreen/index.js
+++ frontend/src/screens/ProductScreen/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Rating from "../../components/Rating";
 import data from "../../data";
@@ -11,7 +12,9 @@ const ProductScreen = (props) => {
   }
 
   return (
-    <div className="row">
+    <>
+    <Link to="/" >Back to results</Link>
+    <div className="row top">
       <div className="col-2">
         <img className="large" src={product.image} alt={product.name} />
       </div>
@@ -66,6 +69,7 @@ const ProductScreen = (props) => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
