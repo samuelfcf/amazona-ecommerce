92772b20963c6a351a2cf351b0a21668325a9159
Samuel Felipe <samuelfelipef@gmail.com>
2021-06-17 18:17:24 -0300

    add info in ProductScreen

--- frontend/src/screens/ProductScreen/index.js
+++ frontend/src/screens/ProductScreen/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React from "react";
 
-const ProductScreen = () => {
+import Rating from "../../components/Rating";
+import data from "../../data";
+
+const ProductScreen = (props) => {
+  const product = data.products.find((x) => x._id === props.match.params.id);
+
+  if (!product) {
+    return <div>Product not found!</div>;
+  }
+
   return (
-    <h1>ProductScreen</h1>
+    <div className="row">
+      <div className="col-2">
+        <img className="large" src={product.image} alt={product.name} />
+      </div>
+
+      <div className="col-1">
+        <ul>
+          <li>
+            <h1>{product.name}</h1>
+          </li>
+          <li>
+            <Rating 
+              rating={product.rating} 
+              numReviews={product.numReviews} 
+            />
+          </li>
+          <li>
+            Price: ${product.price}
+          </li>
+          <li>
+            Description:
+            <p>{product.description}</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col-1">
+        <div className="card card-body">
+          
+          <ul>
+            <li>
+              <div className="row">
+                <div>Price</div>
+                <div className="price">${product.price}</div>
+              </div>
+            </li>
+            <li>
+              <div className="row">
+                <div>Status</div>
+                <div className="price">
+                  {product.countInStock>0
+                    ? (<span className="sucess">In Stock</span>)
+                    : (<span className="error">Unavailable</span>)
+                  }
+                </div>
+              </div>
+            </li>
+            <li>
+              <button className="primary block">Add to Cart</button>
+            </li>
+          
+          </ul>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default ProductScreen;
-
