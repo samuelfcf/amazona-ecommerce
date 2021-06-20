d13262505962bec1b942ab046cbc311d0551710c
Samuel Felipe <samuelfelipef@gmail.com>
2021-06-20 13:35:21 -0300

    create cartActions, cartConstant and cartReducers. Show badge in Cart field in homescreen, save cart in localstorage

--- frontend/src/screens/CartScreen/index.js
+++ frontend/src/screens/CartScreen/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../actions/cartActions';
 
 const CartScreen = (props) => {
 
@@ -8,6 +10,13 @@ const CartScreen = (props) => {
   const qty = props.location.search
               ? Number(props.location.search.split('=')[1])
               : 1
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if(productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty])
 
   return (
     <div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducer';
 
-const initalState = {};
+const initalState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems') 
+                ? JSON.parse(localStorage.getItem('cartItems')) 
+                : []
+  }
+};
 
 // reducer -> especifica como o estado da aplicação irá mudar de acordo com a action que foi enviada para o store.
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
